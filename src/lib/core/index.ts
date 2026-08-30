ca37ea7d81307204465f23ec7ea2a986ac712f01
Juha Itäleino <jsilvanus@gmail.com>
2026-08-30 10:19:45 +0300

    Export reference resolution API

--- src/lib/core/index.ts
+++ src/lib/core/index.ts
@@ -2,5 +2,6 @@ export * from './types';
 export * from './random';
 export * from './pseudonymize';
 export * from './schema';
+export * from './references';
 export * from './prompt';
 export * from './result';
